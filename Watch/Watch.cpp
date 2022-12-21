b209810c386f7d088c1aea6438d9d9fb8b5d0ee8
AlexProstolucky <alexprostolucky@gmail.com>
2022-12-21 17:09:15 +0200

    Добавьте файлы проекта.

---
+++ Watch/Watch.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+class Oclock 
+{
+private:
+    int second = 0;
+    int minute = 0;
+    int hour = 0;
+public:
+    void start() 
+    {
+        while (1) {
+            print();
+            if (second < 59) second++;
+            else { 
+                second = 0;
+                if (minute < 59) minute++;
+                else 
+                {
+                    minute = 0;
+                    hour++;
+                }
+            }
+            Sleep(950);
+        }
+    }
+    void print() 
+    {
+        system("cls");
+        cout << hour << ":" << minute << ":" << second;
+    }
+};
+
+int main()
+{
+    Oclock oclock;
+    oclock.start();
+}
